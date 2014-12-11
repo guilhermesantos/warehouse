62acd0285e13d37144b656222f89fd34074ea5a5
erickzanardo <erickzanardoo@gmail.com>
2014-12-11 13:23:12 -0200

    steam_midweek_madness

---
+++ steam_midweek_madness.js
@@ -0,0 +1,15 @@
+﻿/*
+ Name: Steam midweek madness
+ Description: Busca e envia quais as promoções estão na midweek madness da steam
+ Tags: ['price', 'games', 'Steam']
+ */
+function crawl(html, engine) {
+   var email = '';
+   html.select('.home_area_spotlight').each(function(el) {
+      var link = el.select('.spotlight_img a').attr('href');
+      var img = el.select('.spotlight_img a img').attr('src');
+      var preco = el.select('.discount_final_price').text();
+      email += '<p><a href="' + link + '"><img src="' + img + '"></img></a>' + preco + '</p>';
+   });
+   engine.sendEmail('Steam midweek madness', email);
+}
